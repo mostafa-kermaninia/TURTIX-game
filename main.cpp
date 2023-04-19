c73bf9ae52e599cc6595f0683e6fc04af6fcfb11
mosiyo <mkermani1383@gmail.com>
2023-04-20 00:43:03 +0330

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+   return 0;
+}
